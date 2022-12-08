d3735cfdab72855f16c5ff2df590ca2fa4a04368
ezqy <ezaki@tc3.co.jp>
2022-12-08 09:55:34 +0900

    fix

--- register-github-secret/src/index.ts
+++ register-github-secret/src/index.ts
@@ -1,10 +1,12 @@
 import * as core from "@actions/core";
 import axios from "axios";
-import * as sodium from "libsodium-wrappers";
+//import * as sodium from "libsodium-wrappers"
+const sodium = require("libsodium-wrappers")
 
 const value: string = core.getInput("value");
 const action_repository: string = core.getInput("action_repository");
 const token: string = core.getInput("token");
+const key: string = core.getInput("key")
 
 axios
   .get(
@@ -19,8 +21,9 @@ axios
     }
   )
   .then((res) => {
-    console.log(res.data);
+    const key_id = res.data.key_id
     sodium.ready.then(() => {
+      // https://docs.github.com/ja/rest/actions/secrets?apiVersion=2022-11-28#create-or-update-a-repository-secret
       // Convert Secret & Base64 key to Uint8Array.
       let binkey = sodium.from_base64(res.data.key, sodium.base64_variants.ORIGINAL)
       let binsec = sodium.from_string(value)
@@ -30,8 +33,23 @@ axios
     
       // Convert encrypted Uint8Array to Base64
       let output = sodium.to_base64(encBytes, sodium.base64_variants.ORIGINAL)
-    
-      console.log(output)
-      core.setOutput("encrypted_value", output);
+
+      axios
+        .put(
+          "https://api.github.com/repos/" +
+          action_repository +
+          "/actions/secrets/" + 
+          key,
+          {
+            encrypted_value: output,
+            key_id: key_id
+          },
+          {
+            headers: {
+              Authorization: "Bearer " + token,
+              Accept: "application/vnd.github+json"
+            },
+          }
+        )
     })
   });
