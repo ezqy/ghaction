330f36579e6082ec839baeb5957a243c370fba13
ezqy <ezaki@tc3.co.jp>
2022-12-09 10:18:53 +0900

    aa

--- register-github-secret/src/index.ts
+++ register-github-secret/src/index.ts
@@ -8,11 +8,11 @@ const action_repository: string = core.getInput("action_repository");
 const token: string = core.getInput("token");
 const key: string = core.getInput("key")
 
-if(key == "PRIVATE_KEY"){
-  console.log(value)
-  console.log("--")
-  value = core.getInput("value").replace(/\n/g, '\\n')
-}
+// if(key == "PRIVATE_KEY"){
+//   console.log(value)
+//   console.log("--")
+//   value = core.getInput("value").replace(/\n/g, '\\n')
+// }
 
 console.log(value)
 
